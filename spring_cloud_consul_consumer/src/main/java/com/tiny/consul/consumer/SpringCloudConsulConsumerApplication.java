a6c5de1bf1cb766f5d77aadc0bd8ec6ad449bb13
yiliaohong <yiliaohong@wolaibao.com>
2019-01-23 12:51:38 +0800

    init spring cloud

---
+++ spring_cloud_consul_consumer/src/main/java/com/tiny/consul/consumer/SpringCloudConsulConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.tiny.consul.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulConsumerApplication.class, args);
+    }
+
+}
+
---
+++ spring_cloud_consul_consumer/src/test/java/com/tiny/consul/consumer/SpringCloudConsulConsumerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tiny.consul.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsulConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
